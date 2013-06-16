c450ce9cbd132d4a524a3d898c61f47c5ae470eb
Felix Queiruga <curufinw3@gmail.com>
2013-06-16 15:13:00 +0200

    Modified file to call config generator file and fix permissions on temporary folder

--- zabbix-agent-pkg-generator.sh
+++ zabbix-agent-pkg-generator.sh
@@ -4,21 +4,13 @@
 #
 # TODO: check with regexp if the parameters are well formed IPs or DNS names
 
+CONF_GENERATOR="zabbix-agent-config-generator.sh"
 BASE_FOLDER="zabbix-agent-static-amd64"
 TEMP_FOLDER=".temp"
 PKG_NAME="zabbix-agent-2.0.6.deb"
 CONF_FILE="/usr/share/zabbix-agent/zabbix_agentd.conf"
 
 default_config=false
-
-print_help() {
-    echo "Usage: $0 [OPCIONES]"
-    echo "      -s  | --server          SERVER"
-    echo "      -sa | --server-active   SERVER_IP[:PORT]"
-    echo "      -t  | --timeout         SECONDS (1-30)"
-    echo "      -ra | --refresh-active  SECONDS (60-3600)" 
-    echo "      -h  | --help" 
-}
 
 if [ -f $PKG_NAME ]; then
     rm -f $PKG_NAME
@@ -29,73 +21,21 @@ if [ $# -eq 0 ]; then
     default_config=true
 fi
 
-while [ $# -gt 0 ]; do
-    case $1 in 
-        -s|--server)
-            if [ $2 != "" ]; then
-                server=$2 
-                shift 2
-            else
-                echo "Missing Server IP" 
-                exit 1
-            fi 
-        ;;
-        -sa|--server-active)
-            if [ $2 != "" ]; then
-                server_active=$2
-                shift 2
-            else
-                echo "Missing ServerActive IP"
-                exit 1
-            fi
-        ;;
-        -t|--timeout)
-            if [ $2 != "" -a $2 -ge 1 -a $2 -le 30 ]; then
-                timeout=$2
-                shift 2
-            else
-                echo "Value for Timeout incorrect or missing"
-                exit 1
-            fi
-        ;;
-        -ra|--refresh-active)
-            if [ $2 != "" -a $2 -ge 60 -a $2 -le 3600 ]; then
-                refresh_active=$2
-                shift 2
-            else 
-                echo "Value for RefreshActiveChecks incorrect or missing"
-                exit 1
-            fi
-        ;;
-        -h|--help)
-            print_help
-            exit 0
-        ;;
-        *)
-            echo "Illegal argument $1! Exiting"
-            exit 1
-        ;;
-    esac
-done 
-
 # Create temp folder and modify configuration file 
-cp -r $BASE_FOLDER $TEMP_FOLDER
+if [ -d $TEMP_FOLDER ]; then
+    rm -rf $TEMP_FOLDER
+fi
+cp -rp $BASE_FOLDER $TEMP_FOLDER
 
 if [ $default_config = true ]; then
     echo "Creating package with default configuration"
 else 
-    if [ -n $server ]; then
-        sed "s/^Server=.*/Server=$server/g" -i $TEMP_FOLDER$CONF_FILE
+    # Run the script to modify the configuration file
+    /bin/bash $CONF_GENERATOR $TEMP_FOLDER$CONF_FILE $@    
+    if [ $? -ne 0 ]; then
+        echo "Error while executing $CONF_GENERATOR script" 
+        exit 1
     fi
-    if [ -n $server_active ]; then
-        sed "s/^ServerActive=.*/ServerActive=$server_active/g" -i $TEMP_FOLDER$CONF_FILE
-    fi   
-    if [ -n $timeout ]; then
-        sed "s/^Timeout=.*/Timeout=$timeout/g" -i $TEMP_FOLDER$CONF_FILE
-    fi
-    if [ -n $refresh_active ]; then
-        sed "s/^RefreshActiveChecks=.*/RefreshActiveChecks=$refresh_active/g" -i $TEMP_FOLDER$CONF_FILE
-    fi 
 fi 
 
 # Build the package and delete the temp folder
